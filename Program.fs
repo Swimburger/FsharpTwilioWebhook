0912dc78cfe7c100147dfebe9fae774178f78e58
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-01-27 17:55:45 -0500

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,38 @@
+open System
+open System.Threading
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.HttpOverrides
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Twilio.TwiML
+open Twilio.AspNet.Core
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+
+    builder.Services.AddTwilioRequestValidation() |> ignore
+
+    builder.Services.Configure<ForwardedHeadersOptions>(
+        fun (options: ForwardedHeadersOptions) -> options.ForwardedHeaders <- ForwardedHeaders.All
+    ) |> ignore
+
+    let app = builder.Build()
+
+    app.UseForwardedHeaders() |> ignore
+
+    app.MapPost("/message",Func<HttpRequest, CancellationToken, Task<TwiMLResult>>(fun request ctx ->
+            task {
+                let! (form: IFormCollection) = request.ReadFormAsync(ctx).ConfigureAwait(false)
+                let from = form["From"]
+                return MessagingResponse().Message($"Ahoy {from}!").ToTwiMLResult()
+            })
+        )
+        .ValidateTwilioRequest()
+    |> ignore
+
+    app.Run()
+
+    0 // Exit code
